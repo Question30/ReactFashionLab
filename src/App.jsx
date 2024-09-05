a7f2b568564a34989c7c4ed6fc6c4c3e12d50fcc
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-09-05 16:04:41 -0400

    Lab complete

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import image1 from "./assets/blog-image-1.jpg";
+import image2 from "./assets/blog-image-2.jpg";
+import Header from "./Components/Header";
+import Article from "./Components/Article";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Article
+          imgSrc={image1}
+          imgAlt={"Woman walking off screen to the right"}
+        />
+        <Article
+          imgSrc={image2}
+          imgAlt={"Woman walking off screen to the right"}
+        />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
